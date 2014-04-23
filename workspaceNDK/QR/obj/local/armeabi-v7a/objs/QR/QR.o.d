8852115b093ae50be6c66594a9e6dab1528652f2
Costin <costin@costin-laptop.(none)>
2014-04-23 15:17:57 +0300

    Hello World with OpenCV

---
+++ workspaceNDK/QR/obj/local/armeabi-v7a/objs/QR/QR.o.d
@@ -0,0 +1,13 @@
+obj/local/armeabi-v7a/objs/QR/QR.o: jni/QR.cpp \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/jni.h \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/api-level.h
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/jni.h:
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs.h:
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-9/arch-arm/usr/include/android/api-level.h:
---
+++ workspaceNDK/QR/obj/local/armeabi/objs/QR/QR.o.d
@@ -0,0 +1,13 @@
+obj/local/armeabi/objs/QR/QR.o: jni/QR.cpp \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h \
+ /home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/jni.h:
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs.h:
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/home/costin/Downloads/poli/ndk/android-ndk-r9d/platforms/android-19/arch-arm/usr/include/android/api-level.h:
